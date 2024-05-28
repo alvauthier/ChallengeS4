a460529b578fa7712a7e36b04a991e72149786b6
robin Sobasto <robin.sobasto94@gmail.com>
2024-05-28 14:42:14 +0200

    organization register

---
+++ frontend/lib/register_organization_screen.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class RegisterOrganisationScreen extends StatefulWidget {
+  const RegisterOrganisationScreen({super.key});
+
+  @override
+  _RegisterOrganisationScreenState createState() => _RegisterOrganisationScreenState();
+}
+
+class _RegisterOrganisationScreenState extends State<RegisterOrganisationScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+  final _firstnameController = TextEditingController();
+  final _lastnameController = TextEditingController();
+
+  final RegExp emailRegExp = RegExp(
+      r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+  final RegExp passwordRegExp = RegExp(
+      r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#\$&*~]).{8,}$');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              AppBar(
+                title: const Text(
+                  'S\'inscrire',
+                ),
+                elevation: 0,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        TextFormField(
+                          controller: _firstnameController,
+                          decoration: const InputDecoration(
+                            labelText: 'Nom de l\'organisation',
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Veuillez entrer le nom de l\'organisation';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _firstnameController,
+                          decoration: const InputDecoration(
+                            labelText: 'Description de l\'organisation',
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Veuillez entrer la Description de l\'organisation';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _firstnameController,
+                          decoration: const InputDecoration(
+                            labelText: 'Prénom',
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Veuillez entrer votre prénom';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _lastnameController,
+                          decoration: const InputDecoration(
+                            labelText: 'Nom',
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Veuillez entrer votre nom';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _emailController,
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Veuillez entrer votre email';
+                            } else if (!emailRegExp.hasMatch(value)) {
+                              return 'Veuillez entrer un email valide';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _passwordController,
+                          decoration: const InputDecoration(
+                            labelText: 'Mot de passe',
+                          ),
+                          obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Veuillez entrer votre mot de passe';
+                            } else if (!passwordRegExp.hasMatch(value)) {
+                              return 'Le mot de passe doit contenir au moins 8 caractères, dont une majuscule, une minuscule, un chiffre et un caractère spécial';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _confirmPasswordController,
+                          decoration: const InputDecoration(
+                            labelText: 'Confirmer le mot de passe',
+                          ),
+                          obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Veuillez confirmer votre mot de passe';
+                            }
+                            if (value != _passwordController.text) {
+                              return 'Les mots de passe ne correspondent pas';
+                            }
+                            return null;
+                          },
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 15.0),
+                          child: Container(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                if (_formKey.currentState!.validate()) {
+                                  // Process data.
+                                  try {
+                                    var response = await http.post(
+                                      Uri.parse('http://10.0.2.2:8080/register'), // 10.0.2.2 => localhost
+                                      headers: <String, String>{
+                                        'Content-Type': 'application/json; charset=UTF-8',
+                                      },
+                                      body: jsonEncode(<String, String>{
+                                        'firstname': _firstnameController.text,
+                                        'lastname': _lastnameController.text,
+                                        'email': _emailController.text,
+                                        'password': _passwordController.text,
+                                      }),
+                                    );
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text(response.statusCode == 201
+                                            ? 'Inscription réussie'
+                                            : 'Erreur lors de l\'inscription'),
+                                        duration: Duration(seconds: 5),
+                                      ),
+                                    );
+                                    if (response.statusCode == 201) {
+                                      Navigator.pop(context);
+                                    }
+                                  } catch (e) {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content: Text('Une erreur est survenue. Veuillez vérifier votre connexion internet ou réessayer plus tard.'),
+                                        duration: Duration(seconds: 5),
+                                      ),
+                                    );
+                                  }
+                                }
+                              },
+                              child: const Text('S\'inscrire'),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}

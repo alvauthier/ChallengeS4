704e5cab91e93bc179c70fcd51e08510a575d66e
Alexandre Vauthier <alex.vauthier94@gmail.com>
2024-07-02 15:57:16 +0200

    change concert_category model

--- backend/internal/models/concert_category.go
+++ backend/internal/models/concert_category.go
@@ -8,15 +8,16 @@ import (
 
 type ConcertCategory struct {
 	// gorm.Model
-	ID         uuid.UUID `gorm:"unique;type:uuid;primaryKey"`
-	ConcertId  uuid.UUID `gorm:"type:uuid;not null;uniqueIndex:idx_concert_category"`
-	CategoryId int       `gorm:"not null;uniqueIndex:idx_concert_category"`
-	NbTickets  int       `gorm:"not null;uniqueIndex:idx_concert_category"`
-	Price      float64   `gorm:"not null;uniqueIndex:idx_concert_category"`
-	Tickets    []Ticket
-	Concert    Concert  `gorm:"foreignKey:ConcertId"`
-	Category   Category `gorm:"foreignKey:CategoryId"`
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
-	DeletedAt  *time.Time `gorm:"index"`
+	ID               uuid.UUID `gorm:"unique;type:uuid;primaryKey"`
+	ConcertId        uuid.UUID `gorm:"type:uuid;not null;uniqueIndex:idx_concert_category"`
+	CategoryId       int       `gorm:"not null;uniqueIndex:idx_concert_category"`
+	AvailableTickets int       `gorm:"not null;uniqueIndex:idx_concert_category"`
+	SoldTickets      int       `gorm:"uniqueIndex:idx_concert_category"`
+	Price            float64   `gorm:"not null;uniqueIndex:idx_concert_category"`
+	Tickets          []Ticket
+	Concert          Concert  `gorm:"foreignKey:ConcertId"`
+	Category         Category `gorm:"foreignKey:CategoryId"`
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+	DeletedAt        *time.Time `gorm:"index"`
 }

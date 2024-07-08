8095c712eaebac44a9261b452b52f1b08622acb9
MelvinCourant <melvin.courant1@gmail.com>
2024-07-08 11:00:46 +0200

    :lipstick: update categories list to use radio button and add button to proceed paiement

--- frontend/lib/booking_screen.dart
+++ frontend/lib/booking_screen.dart
@@ -14,6 +14,7 @@ class BookingScreen extends StatefulWidget {
 class _BookingScreenState extends State<BookingScreen> {
   final storage = const FlutterSecureStorage();
   String? email;
+  int? selectedCategory; // Variable to store the selected category
 
   @override
   Widget build(BuildContext context) {
@@ -21,29 +22,54 @@ class _BookingScreenState extends State<BookingScreen> {
         backgroundColor: Colors.white,
         body: Stack(
             children: [
-              ListView.builder(
-                itemCount: widget.concertCategories.length,
-                itemBuilder: (context, index) {
-                  var concertCategory = widget.concertCategories[index];
-                  return Card(
-                    child: ListTile(
-                      title: Text(concertCategory.category.name.toString()),
-                      subtitle: Text('Prix: ${concertCategory.price} €, Nombre de tickets: ${concertCategory.numberTickets}'),
-                      trailing: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(6.0),
-                          ),
-                          backgroundColor: Colors.deepOrange,
-                        ),
-                        onPressed: () {
-                        },
-                        child: const Text('Choisir'),
+              Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 80.0, bottom: 20.0),
+                    child: Text(
+                      'Choisissez votre catégorie de billets',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Readex Pro',
+                          fontWeight: FontWeight.w600
                       ),
                     ),
-                  );
-                },
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: widget.concertCategories.length,
+                      itemBuilder: (context, index) {
+                        var concertCategory = widget.concertCategories[index];
+                        return Card(
+                          child: RadioListTile<int>(
+                            title: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(concertCategory.category.name.toString()),
+                                Text(
+                                  '${concertCategory.price} €',
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 20,
+                                      color: Colors.deepOrangeAccent
+                                  ),
+                                ),
+                              ],
+                            ),
+                            subtitle: Text('Nombre de tickets: ${concertCategory.numberTickets}'),
+                            value: concertCategory.category.id,
+                            groupValue: selectedCategory,
+                            onChanged: (int? value) {
+                              setState(() {
+                                selectedCategory = value;
+                              });
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
               Positioned(
                 top: 10.0,
@@ -54,7 +80,30 @@ class _BookingScreenState extends State<BookingScreen> {
                 ),
               ),
             ]
-        )
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // TODO: Implement payment logic here
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6.0),
+                ),
+                backgroundColor: Colors.deepOrange,
+              ),
+              child: const Text(
+                'Procéder au paiement',
+                style: TextStyle(
+                    fontFamily: 'Readex Pro',
+                    color: Colors.white
+                ),
+              ),
+            ),
+          ),
+        ),
     );
   }
 }

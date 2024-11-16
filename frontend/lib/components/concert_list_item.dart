ec34958122a66cd609e4552b0ce139b7a60b3f39
Alexandre Vauthier <alex.vauthier94@gmail.com>
2024-11-16 19:17:58 +0100

    fix queue not working

--- frontend/lib/components/concert_list_item.dart
+++ frontend/lib/components/concert_list_item.dart
@@ -1,14 +1,18 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:go_router/go_router.dart';
 import 'package:weezemaster/core/models/concert.dart';
 import 'package:intl/intl.dart';
+import 'package:weezemaster/core/services/token_services.dart';
+import 'package:weezemaster/core/services/websocket_service.dart';
 
 class ConcertListItem extends StatelessWidget {
   final Concert concert;
   final bool showArtistName;
 
-  const ConcertListItem({super.key, required this.concert, this.showArtistName = true});
+  ConcertListItem({super.key, required this.concert, this.showArtistName = true});
 
   String formatDate(String date) {
     DateTime dateTime = DateTime.parse(date);
@@ -16,14 +20,86 @@ class ConcertListItem extends StatelessWidget {
     return dateFormat.format(dateTime);
   }
 
+  final webSocketService = WebSocketService();
+  
+  Future<void> joinQueueOrConcertPage(BuildContext context, String concertId, String userId) async {
+    webSocketService.connect(concertId, userId);
+
+    // Accès au flux de diffusion
+    final broadcastStream = webSocketService.stream;
+    
+    if (broadcastStream == null) {
+      debugPrint('Failed to get WebSocket broadcast stream.');
+      return;
+    }
+
+    broadcastStream.listen(
+      (event) {
+        final data = jsonDecode(event);
+
+        if (data['isFirstMessage'] == true && data['status'] == 'access_granted') {
+          context.pushNamed(
+            'concert',
+            pathParameters: {'id': concertId},
+            extra: {
+              'webSocketService': webSocketService,
+            },
+          );
+        } else if (data['isFirstMessage'] == true && data['status'] == 'in_queue') {
+          context.pushNamed(
+            'queue',
+            extra: {
+              'position': data['position'],
+              'webSocketService': webSocketService,
+            },
+          );
+        }
+      },
+      onError: (error) {
+        debugPrint('WebSocket error: $error');
+      },
+      onDone: () {
+        debugPrint('WebSocket connection closed.');
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        context.pushNamed(
-          'concert',
-          pathParameters: {'id': concert.id},
-        );
+      onTap: () async {
+        final tokenService = TokenService();
+        String? token = await tokenService.getValidAccessToken();
+        if (token == null) {
+          context.pushNamed(
+            'concert',
+            pathParameters: {'id': concert.id},
+          );
+        } else {
+          final parts = token.split('.');
+          if (parts.length != 3) {
+            throw Exception('Invalid token');
+          }
+
+          String output = parts[1].replaceAll('-', '+').replaceAll('_', '/');
+          switch (output.length % 4) {
+            case 0:
+              break;
+            case 2:
+              output += '==';
+              break;
+            case 3:
+              output += '=';
+              break;
+            default:
+              throw Exception('Illegal base64url string!"');
+          }
+
+          String userId = json.decode(utf8.decode(base64.decode(output)))['id'];
+
+          debugPrint('Joining queue or concert page for concert: ${concert.id} and user: $userId');
+          await joinQueueOrConcertPage(context, concert.id, userId);
+        }
       },
       child: Padding(
         padding: const EdgeInsets.all(10.0),
